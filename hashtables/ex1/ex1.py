f24c365660bb72f2a6015b57416047e71c73893a
toricruzmendiola <mtoricruz@gmail.com>
2020-08-07 09:27:34 -0700

    mvp complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,14 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    d = {}
+
+    for i in range(length):
+        curr = weights[i]
+        # check if current weight is in the cache/dict
+        if curr in d:
+            # if it is, set prev to the curr value 
+            prev = d[curr]
+            return (i, prev)
+        # now hash key is the smaller weight
+        d[limit - weights[i]] = i 
 
     return None
